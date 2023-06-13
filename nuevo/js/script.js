9fd035a4687b8b348351b39826ba67fb5c23d2fb
aroc780 <aroc780@gmail.com>
2023-06-13 06:39:57 -0400

    pro1

---
+++ nuevo/js/script.js
@@ -0,0 +1,41 @@
+document.querySelector("#login-btn").onclick = () =>{
+    login.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector('#menu-btn').onclick = () =>{
+    login.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    login.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".planetas", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView:1,
+        },
+        700:{
+            slidesPerView:2,
+        },
+    }
+})
+document.querySelectorAll('.small-img').forEach(image =>{
+    image.addEventListener('click', () =>{
+        var src = image.getAttribute('src');
+        document.querySelector('.big-img').src = src;
+    });
+});
